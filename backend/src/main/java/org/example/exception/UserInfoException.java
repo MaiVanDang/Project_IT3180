1b8258b6e1c76bc5e563410e6f4810c8b44ab2dd
MaiVanDang <dangmaivan24@gmail.com>
2025-04-29 05:45:14 +0700

    exception

---
+++ backend/src/main/java/org/example/exception/UserInfoException.java
@@ -0,0 +1,8 @@
+package org.example.exception;
+
+//Handles the exception that the parameter value does not exist is passed into the database for all entities
+public class UserInfoException extends Exception {
+    public UserInfoException(String message){
+        super(message);
+    }
+}
