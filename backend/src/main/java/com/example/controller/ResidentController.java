c5bef0927296e2cb6e1887216f991475c1559a97
BaoHust2004 <buibaomoyu@gmail.com>
2025-06-07 10:37:02 +0700

    update residentController

--- backend/src/main/java/com/example/controller/ResidentController.java
+++ backend/src/main/java/com/example/controller/ResidentController.java
@@ -34,13 +34,12 @@ public class ResidentController {
         return ResponseEntity.status(HttpStatus.OK).body(residentResponses);
     }
 
-    @GetMapping("/all")
-    public ResponseEntity<PaginatedResponse<Resident>> getAll(@Filter Specification<Resident> spec,
-            @RequestParam(value = "page", defaultValue = "1") int page,
-            @RequestParam(value = "size", defaultValue = "10") int size) {
-        Pageable pageable = PageRequest.of(page - 1, size);
-        PaginatedResponse<Resident> residentResponses = this.residentService.fetchAll(spec, pageable);
-        return ResponseEntity.status(HttpStatus.OK).body(residentResponses);
+    
+    // Create new resident
+    @PostMapping("")
+    public ResponseEntity<Resident> createNewUser(@Valid @RequestBody ResidentCreateRequest apiResident) {
+        Resident resident = this.residentService.createResident(apiResident);
+        return new ResponseEntity<>(resident, HttpStatus.CREATED);
     }
 
     // fetch resident by id
@@ -50,13 +49,6 @@ public class ResidentController {
         return ResponseEntity.status(HttpStatus.OK).body(fetchResident);
     }
 
-    // Create new resident
-    @PostMapping("")
-    public ResponseEntity<Resident> createNewUser(@Valid @RequestBody ResidentCreateRequest apiResident) {
-        Resident resident = this.residentService.createResident(apiResident);
-        return new ResponseEntity<>(resident, HttpStatus.CREATED);
-    }
-
     // Delete resident by id
     @DeleteMapping("/{id}")
     public ResponseEntity<ApiResponse<String>> deleteResident(@PathVariable("id") long id) throws Exception {
@@ -64,11 +56,22 @@ public class ResidentController {
         return ResponseEntity.ok(response);
     }
 
+
     // Update resident
     @PutMapping("")
     public ResponseEntity<Resident> updateUser(@RequestBody ResidentUpdateRequest apiResident) throws Exception {
         Resident resident = this.residentService.updateResident(apiResident);
         return ResponseEntity.ok(resident);
     }
+
+    @GetMapping("/all")
+    public ResponseEntity<PaginatedResponse<Resident>> getAll(@Filter Specification<Resident> spec,
+            @RequestParam(value = "page", defaultValue = "1") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+        PaginatedResponse<Resident> residentResponses = this.residentService.fetchAll(spec, pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(residentResponses);
+    }
+    
 
 }
